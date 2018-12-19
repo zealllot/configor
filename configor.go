5250708b878ed930ad2cc810974ceba571cd43f7
Zealllot <502372679@qq.com>
2018-12-19 16:06:58 +0800

    add load config

--- configor.go
+++ configor.go
@@ -2,24 +2,27 @@ package configor
 
 import (
 	"errors"
-	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"path"
 	"path/filepath"
 
-	"io/ioutil"
-
 	"github.com/fsnotify/fsnotify"
 )
 
-func Load(configPath string) {
-	go addConfigPath(configPath)
+func Load(configPath string) (kvMap *map[string]interface{}) {
+	newMap := make(map[string]interface{})
+
+	go addConfigPath(configPath, &newMap)
+
+	kvMap = &newMap
 	log.Println("Path: ", configPath)
 	log.Println("Initial configor...")
+	return
 }
 
-func addConfigPath(configPath string) (err error) {
+func addConfigPath(configPath string, kvMap *map[string]interface{}) (err error) {
 	fd, err := os.Stat(configPath)
 	if err != nil {
 		return
@@ -36,6 +39,11 @@ func addConfigPath(configPath string) (err error) {
 		absPath = configPath
 	}
 
+	*kvMap, err = loadConfig(absPath)
+	if err != nil {
+		return
+	}
+
 	dirPath := path.Dir(configPath)
 
 	watcher, err := fsnotify.NewWatcher()
@@ -49,31 +57,46 @@ func addConfigPath(configPath string) (err error) {
 			select {
 			case event, ok := <-watcher.Events:
 				if !ok {
-					fmt.Println("return")
+					log.Println("return")
 					return
 				}
 				if event.Name == absPath {
 					switch event.Op {
 					case fsnotify.Create:
 						log.Println(event.Op)
-						loadConfig(absPath)
+						*kvMap, err = loadConfig(absPath)
+						if err != nil {
+							return
+						}
 					case fsnotify.Remove:
 						log.Println(event.Op)
-						loadConfig(absPath)
+						*kvMap, err = loadConfig(absPath)
+						if err != nil {
+							return
+						}
 					case fsnotify.Write:
 						log.Println(event.Op)
-						loadConfig(absPath)
+						*kvMap, err = loadConfig(absPath)
+						if err != nil {
+							return
+						}
 					case fsnotify.Chmod:
 						log.Println(event.Op)
-						loadConfig(absPath)
+						*kvMap, err = loadConfig(absPath)
+						if err != nil {
+							return
+						}
 					case fsnotify.Rename:
 						log.Println(event.Op)
-						loadConfig(absPath)
+						*kvMap, err = loadConfig(absPath)
+						if err != nil {
+							return
+						}
 					}
 				}
 			case err, ok := <-watcher.Errors:
 				if !ok {
-					fmt.Println("return")
+					log.Println("return")
 					return
 				}
 				log.Println("error:", err)
@@ -89,13 +112,66 @@ func addConfigPath(configPath string) (err error) {
 	return
 }
 
-func loadConfig(path string) (content string, err error) {
+func loadConfig(path string) (kvMap map[string]interface{}, err error) {
 	bytesFile, err := ioutil.ReadFile(path)
 	if err != nil {
 		return
 	}
-	content = string(bytesFile)
-	log.Println(`Content: 
-` + content)
+	var key []byte
+	var value []byte
+	var readKey bool
+	var hasRead bool
+	kvMap = make(map[string]interface{})
+	readKey = true
+	for _, v := range bytesFile {
+		if readKey {
+			if v == '\n' {
+				err = errors.New("Incorrect config file! ")
+				return
+			}
+			if len(key) > 0 && v == ' ' {
+				hasRead = true
+				continue
+			}
+			if v == '=' {
+				readKey = false
+				hasRead = false
+				continue
+			}
+			if hasRead && v != ' ' {
+				err = errors.New("Incorrect config file! ")
+				return
+			}
+			if v != ' ' && !hasRead {
+				key = append(key, v)
+				continue
+			}
+		} else {
+			if v == '=' {
+				err = errors.New("Incorrect config file! ")
+				return
+			}
+			if len(value) > 0 && v == ' ' {
+				hasRead = true
+				continue
+			}
+			if v == '\n' {
+				readKey = true
+				hasRead = false
+				kvMap[string(key)] = string(value)
+				key = []byte{}
+				value = []byte{}
+				continue
+			}
+			if hasRead && v != ' ' {
+				err = errors.New("Incorrect config file! ")
+				return
+			}
+			if v != ' ' && !hasRead {
+				value = append(value, v)
+				continue
+			}
+		}
+	}
 	return
 }
